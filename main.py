5153ce6988e51e0d503fc318db47d2a5a237c418
Weiqifan <cmlykke@hotmail.com>
2018-11-30 19:02:11 +0100

    pictures added

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import numpy
+import matplotlib
+import cv2
+
+# change python path:
+# https://stackoverflow.com/questions/43351596/activating-anaconda-environment-in-vscode
+# files -> preferences -> settings -> Python Configuration(82):
+# "python.pythonPath": "C:\\Anaconda3\\envs\\py34\\python.exe"
+
+# https://code.visualstudio.com/docs/python/environments
+# Python: Select Interpreter
+
+
+print("Command Palette (Ctrl+Shift+P).")
+import sys
+print(sys.version_info)
+
+
+print("hel;lo")
