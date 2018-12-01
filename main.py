97d489abb1392fa8884e7a7671644a94d9d5844a
Weiqifan <cmlykke@hotmail.com>
2018-12-01 10:42:57 +0100

    fredagsaendringer

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 
-import numpy
-import matplotlib
+import sys
+print(sys.version_info)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
 import cv2
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+print("hello")
+
+#import pic
 
 # change python path:
 # https://stackoverflow.com/questions/43351596/activating-anaconda-environment-in-vscode
@@ -11,10 +25,10 @@ import cv2
 # https://code.visualstudio.com/docs/python/environments
 # Python: Select Interpreter
 
+#img = cv2.imread('pic/pic1WonB.png', cv2.IMREAD_GRAYSCALE)
 
-print("Command Palette (Ctrl+Shift+P).")
-import sys
-print(sys.version_info)
-
-
-print("hel;lo")
+#vid 1 - ca. 10.00 virker
+# https://www.youtube.com/watch?v=Z78zbnLlPUA&list=PLQVvvaa0QuDdttJXlLtAJxJetJcqmqlQq
+#cv2.imshow('graa', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
