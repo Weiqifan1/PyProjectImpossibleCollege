8aabc07af461623e63156c3b921c0f35cbb102c8
Weiqifan <cmlykke@hotmail.com>
2018-12-02 19:43:39 +0100

    test

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ image = cv2.imread("pic/pic2W.jpg")
 #crop = get_still_filter.text_image_black_white("pic/pic2W.jpg", [[0,0,255],[255,255,255]])
 #chrsave = get_still_filter.read_pic_save_text(crop,"test_results/mypic.png" ,"test_results/mynew.txt")
 
-
+print("hey")
 
 #cv2.imshow("contour", crop)
 cv2.imshow("image", image)
