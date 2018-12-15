2b264ab12ea4bcabfafbb33c6d1dcaf02db90b3d
Bo Henriksen <bomarconi@gmail.com>
2018-12-15 15:02:32 +0100

    Create forlders for audio and subtitles if it does not exist.

--- library/preparetion/empty_folders.py
+++ library/preparetion/empty_folders.py
@@ -2,9 +2,13 @@ import os
 
 def empty_folders_before_run():
 
-    folder = 'library/subtitles'
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
+    folder_subtitles = 'library/subtitles'
+
+    # Create a folder if it does not exist.
+    create_folder(folder_subtitles)
+
+    for the_file in os.listdir(folder_subtitles):
+        file_path = os.path.join(folder_subtitles, the_file)
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
@@ -12,13 +16,23 @@ def empty_folders_before_run():
         except Exception as e:
             print(e)
     
-    folder = 'library/audio'
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
+    folder_audio = 'library/audio'
+    create_folder(folder_audio)
+
+    for the_file in os.listdir(folder_audio):
+        file_path = os.path.join(folder_audio, the_file)
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
             # elif os.path.isdir(file_path): shutil.rmtree(file_path)
         except Exception as e:
             print(e)
-    
+
+
+def create_folder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)  
+  
