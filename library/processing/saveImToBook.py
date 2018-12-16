2e6253ac28a7f3f5e7e36f73075e07b9aaf2a9c9
Weiqifan1 <cmlykke@hotmail.com>
2018-12-16 18:42:32 +0100

    ubrugelige still image .py fil fjernet

--- library/processing/saveImToBook.py
+++ library/processing/saveImToBook.py
@@ -4,15 +4,10 @@ import pytesseract
 import argparse
 import cv2
 import os
-# from library import get_still_filter # BRUGES IKKE! HUSK AT SLETTE FIL!!!!
 import library.processing.simpel_video_filter as simpel_video_filter
-import shutil # BRUGES IKKE!!
 import library.translation_and_speech.translate as translate
 import library.translation_and_speech.text_to_speech as text_to_speech
 import library.translation_and_speech.audio as audio
-
-# 2018-12-09 -- opgave til imorgen - rens noise straksf efter hvide farver er fundet
-# python saveImToBook.py
 
 
 def rens2(image):
@@ -128,43 +123,8 @@ def capture_video(translation_language):
 # , 
                 text_to_speech.run_translate(len(linesFromF), translation_language)
                 audio.speak(len(linesFromF))
-                # print(list_of_ylevels)
-
-                # lav foelgende beregning:
-                # tag gennemsnittet af de 3 sidste midY der har en string stoerre end 0
-                # hvis result ligger inden for +-60 pixels
-                # last_three_subs
-
-            '''
-            for strItem in list_of_texts:
-                if len(strItem) > 0:
-                    pure_string.append(strItem)
-            if len(pure_string) > 3:
-                for numItem in pure_string:
-                    idx = list_of_texts.index(numItem)
-                    pure_midY.append(saved_midY[idx])
-            print("pS:       " + str(pure_string))
-            print("pN:       " + str(pure_midY))
-            '''
-
-            '''
-            pic_path = "book/frame_"+str(count)+".jpg"   #"pic/pic2W.jpg"
-            filename = pic_path.format(os.getpid())
-            cv2.imwrite(filename, frame)
-
-            pic_path = "book/frame_"+str(count)+"_basic.jpg"   #"pic/pic2W.jpg"
-            filename = pic_path.format(os.getpid())
-            cv2.imwrite(filename, basic)
-
-            pic_path = "book/frame_"+str(count)+"_cont.jpg"   #"pic/pic2W.jpg"
-            filename = pic_path.format(os.getpid())
-            cv2.imwrite(filename, cont)
 
 
-            # pic_path = "book/frame_"+str(count)+"_mask.jpg"   #"pic/pic2W.jpg"
-            # filename = pic_path.format(os.getpid())
-            # cv2.imwrite(filename, mask)
-            '''
             if (count > 700):
                 break
 
@@ -172,177 +132,7 @@ def capture_video(translation_language):
             break
         count += 1  # count frame
 
-    # note start
     file.close()
-    # note end
     cap.release()
     cv2.destroyAllWindows()
 
-
-print("end")
-
-
-######################################
-# Chr's foerste videokode
-#########################################
-'''
-# cap = cv2.VideoCapture('pic/girl.webm')
-cap = cv2.VideoCapture('videoplayback.webm')
-# note start
-
-file = open("film.txt", "w")
-file.write("")
-file.close()
-file = open("film.txt", "a+", encoding="utf-8")#"w")
-# note end
-count = 0 #count frames
-oldline = ""
-while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    ######
-    # time = cv2.VideoCapture.get(CV_CAP_PROP_POS_AVI_RATIO)
-    # print(time)
-    # print("\n")
-    if (count%50 == 0):
-        try:
-            cont = get_still_filter.text_image_black_white(
-                frame, [[0,0,255],[255,255,255]])
-            clean_cont =  get_still_filter.cleaning_subs(cont)
-            text = pytesseract.image_to_string(clean_cont, lang="dan")
-            line = text+"\n"
-            if line == oldline:
-                pass
-            else:
-                oldline = line
-                file.write(line)
-                print(text)
-        except:
-            pass
-
-    # except expression as identifier:
-    #    pass
-    #######
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    count += 1 #count frame
-
-# note start
-file.close()
-# note end
-cap.release()
-cv2.destroyAllWindows()
-'''
-
-######################################
-# Chr's stillebillede kode
-#########################################
-'''
-"pic1WonB.png"
-"pic2W.jpg"
-image = cv2.imread("pic/pic2W.jpg")
-cont = get_still_filter.text_image_black_white(
-    image, [[0,0,255],[255,255,255]])
-clean_cont =  get_still_filter.cleaning_subs(cont)
-text = pytesseract.image_to_string(clean_cont)
-print(text)  #debugging
-file = open("test_results/mynew.txt", "w")
-file.write(text)
-file.close()
-# get_still_filter.read_pic_save_text(clean_cont, "test_results/binary.png" ,"test_results/mynew.txt")
-
-cv2.imshow("image", image)
-cv2.imshow("cont", cont)
-cv2.imshow("clean_cont", clean_cont)
-
-cv2.waitKey(0)
-'''
-
-
-######################################
-# Bo's video kode:
-#########################################
-'''
-import numpy as np
-import cv2
-# import pytesseract # pip install pytesseract
-
-# opencv and tesser ocr
-# https://docs.opencv.org/3.0-beta/modules/text/doc/ocr.html
-
-# download movie from youtube. Just give it the path.
-# https://video.genyoutube.net/86d7jx2YB0Y
-
-cap = cv2.VideoCapture('pic/girl.webm')
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(
-    nmixtures=55, backgroundRatio=0.005, noiseSigma=9)
-
-while(1):
-    kernel = np.ones((1, 1), np.uint8)
-    kernel_closing = np.ones((2, 2), np.uint8)
-
-    ret, frame = cap.read()
-
-    cropped = frame[283:600, 100:600]
-
-    # fgmask = fgbg.apply(cropped)
-
-    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)[1]
-
-    cv2.imshow('gray', gray)
-
-    get_still_filter.read_pic_save_text(
-        gray, "test_results/mypic.png", "test_results/mynew.txt")
-
-    # erosion = cv2.morphologyEx(fgmask, cv2.MORPH_ERODE, kernel)
-    # closing = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel_closing)
-    # erosion = cv2.morphologyEx(closing, cv2.MORPH_ERODE, kernel)
-
-    # thresh = cv2.threshold(erosion, 140, 255, cv2.THRESH_BINARY)[1]
-
-    # dilate = cv2.dilate(fgmask, None, iterations=1)
-    # closing = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
-
-    # dilation = cv2.dilate(fgmask, kernel, iterations=1)
-    # closing_stepwise = cv2.erode(dilation, kernel, iterations=1)
-    # assert np.array_equal(closing, closing_stepwise)
-
-    # show the frame
-    # cv2.imshow('cropped', cropped)
-    """ cv2.imshow('fgmask', fgmask)
-    cv2.imshow('closing_stepwise', closing_stepwise)
-    cv2.imshow('closing', closing)
-    cv2.imshow('end', closing_stepwise) """
-    # cv2.imshow('threshold', thresh) # Ikke rigtig nogen forskel.
-    # cv2.imshow('dilate', dilate) # Hele teksten bliver hvid, og bogstaverne går over i hinanden.
-    # print(pytesseract.image_to_string(fgmask))
-
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-
-def process_frame(fgmask):
-    # compute the absolute difference between the current frame and
-    # first frame
-    # frame_delta = cv.absdiff(fgmask, gray) * 1 #amp
-    thresh = cv2.threshold(fgmask, 40, 255, cv2.THRESH_BINARY)[1]
-
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-
-    return thresh  # , frame_delta
-'''
-
-
-# end
