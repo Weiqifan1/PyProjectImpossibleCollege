d0cea6049c8b1dabb0b4550963ca2841f780cc13
Bo Henriksen <bomarconi@gmail.com>
2018-12-19 13:39:42 +0100

    Refacturet create_files.py

--- library/preparetion/create_files.py
+++ library/preparetion/create_files.py
@@ -4,9 +4,8 @@ def create_files_for_subtitles():
     """ 
     This function creates 3 empty files for our subtitles.
     """
-    f = open("data/output/subtitles/frames_and_subtitles.txt", "w+", encoding="utf-8")
-    f.close()
-    f = open("data/output/subtitles/subtitle_from_movie.txt", "w+", encoding="utf-8")
-    f.close()
-    f = open("data/output/subtitles/translated_subtitles.txt", "w+", encoding="utf-8")
-    f.close()
+    path_to_empty_file = ['data/output/subtitles/frames_and_subtitles.txt', 'data/output/subtitles/subtitle_from_movie.txt', 'data/output/subtitles/translated_subtitles.txt']
+    
+    for element in path_to_empty_file:
+        with open(element, 'w+', encoding="utf-8") as file:
+            file.write('')
